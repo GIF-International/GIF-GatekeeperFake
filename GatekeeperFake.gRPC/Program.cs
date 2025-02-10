90aa2d9758146b7ed9c7acf0cab19ddbbf69f55b
RodrigoZanferrari <rzanferrari@gifconsulting.com>
2025-02-10 10:05:27 -0300

    Arquivos iniciais - GatekeeperFake.gRPC

---
+++ GatekeeperFake.gRPC/Program.cs
@@ -0,0 +1,24 @@
+using GatekeeperFake.gRPC.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddGrpc(options =>
+{
+    options.EnableDetailedErrors = true;
+});
+
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+}
+
+app.MapGrpcService<GreeterService>();
+app.MapGrpcService<SessionsManagerService>();
+
+//app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+app.Run();
---
+++ GatekeeperFake.gRPC/Services/SessionsManagerService.cs
@@ -0,0 +1,43 @@
+﻿using Nuance.Biosec.V1.Sessions;
+using static Nuance.Biosec.V1.Sessions.SessionsManager;
+using grpc = global::Grpc.Core;
+
+namespace GatekeeperFake.gRPC.Services
+{
+    public class SessionsManagerService : SessionsManagerBase
+    {
+        private readonly ILogger<SessionsManagerService> _logger;
+
+        public SessionsManagerService(ILogger<SessionsManagerService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override global::System.Threading.Tasks.Task<global::Nuance.Biosec.V1.Sessions.StartEngagementResponse> StartEngagement(global::Nuance.Biosec.V1.Sessions.StartEngagementRequest request, grpc::ServerCallContext context)
+        {
+            //throw new grpc::RpcException(new grpc::Status(grpc::StatusCode.Unimplemented, ""));
+
+            var task = Task.FromResult(new StartEngagementResponse
+            {
+                GkEngagementId = new Nuance.Biosec.V1.UniqueId { Value = Guid.NewGuid().ToString() },
+                Status = new Nuance.Rpc.Status { StatusCode = Nuance.Rpc.StatusCode.Ok },
+            });
+
+            return task;
+        }
+
+        public override global::System.Threading.Tasks.Task<global::Nuance.Biosec.V1.Sessions.StartSessionResponse> StartSession(global::Nuance.Biosec.V1.Sessions.StartSessionRequest request, grpc::ServerCallContext context)
+        {
+            //throw new grpc::RpcException(new grpc::Status(grpc::StatusCode.Unimplemented, ""));
+
+            var task = Task.FromResult(new StartSessionResponse()
+            {
+                GkEngagementId = new Nuance.Biosec.V1.UniqueId { Value = Guid.NewGuid().ToString() },
+                GkSessionId = new Nuance.Biosec.V1.UniqueId { Value = Guid.NewGuid().ToString() },
+                Status = new Nuance.Rpc.Status { StatusCode = Nuance.Rpc.StatusCode.Ok },
+            });
+
+            return task;
+        }
+    }
+}
